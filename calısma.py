129f08a28391f78380b8644d06db451c54eb5a04
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-24 12:33:08 +0300

    Delete calısma.py

--- calısma.py
+++
@@ -1,6 +0,0 @@
-boy = float(input("Lütfen boyunuzu giriniz : "))
-kilo = int(input("Lütfen kilonuzu giriniz : "))
-
-BedenKitleIndeksi = kilo / boy 
-
-print("Beden Kitle İndeksi : ", BedenKitleIndeksi)
